691b6078750128a9b5706adb9524a8c1b388e7c0
Zoe <nickzy.bg@gmail.com>
2024-06-30 21:44:59 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Description from "./Components/Description.tsx";
+
+function App() {
+    return (
+        <Description />
+    )
+}
+
+export default App
---
+++ src/Components/Description.tsx
@@ -0,0 +1,11 @@
+import './Description.css'
+
+function Description() {
+    return (
+        <div id ="description">
+            <h1>Automatically add a song to your Spotify playlist with just a YouTube link</h1>
+        </div>
+    )
+}
+
+export default Description;
