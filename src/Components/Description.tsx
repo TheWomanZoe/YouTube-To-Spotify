5fafed72cecbceabcb614ed6a138c5a80a20a89c
Zoe <nickzy.bg@gmail.com>
2024-07-01 23:30:28 +0300

    Updated Description.tsx/css

--- src/Components/Description.tsx
+++ src/Components/Description.tsx
@@ -3,7 +3,8 @@ import './Description.css'
 function Description() {
     return (
         <div id ="description">
-            <h1>Automatically add a song to your Spotify playlist with just a YouTube link</h1>
+            <h1>SpotTube</h1>
+            <p>Automatically add a song to your Spotify playlist with just a YouTube link</p>
         </div>
     )
 }
